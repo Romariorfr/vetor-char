46321bcf95b86c98d7a1b53c526cecdc11c110ea
Romario Ferreira <romarioferreirarezende@hotmail.com>
2020-06-29 18:47:02 -0300

    projeto criado

---
+++ criacao_vetor.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/*
+	Romario F.  Data: 29/06/2020
+	
+	criando um vetor char em linguagem C e addicionando 
+	dados pelo teclado,depois exibindo o resultado do 
+	ultimo nome digitado para o primeiro.
+*/
+
+int main(){
+	//variaveis
+	int caracter = 20;
+	int nomes = 10;  
+	char lista_nomes[caracter][nomes];
+	
+	printf("*Lista de nomes, digite 10 nomes*\n");
+	for(int i=1; i<=nomes; i++){
+		printf("nome %d : ",i);
+		scanf("%s",lista_nomes[i]); // o vetor ja e o propio ponteiro
+		fflush(stdin); //limpa o buffer do teclado
+	}
+	printf("\n\n");//espacamento
+	
+	printf("*Os nomes digitados de tras para frente*\n");
+	for(int j=10; j>=1; j--){
+		printf("%d nome digitado: %s\n",j,lista_nomes[j]);
+				
+	}
+		
+}
